da9053050680037d2d15908dcafdd46071728cb4
Samuel Laheux <samuel.laheux@novadiscovery.com>
2020-03-31 09:49:31 +0200

    feat(intro): forms challenge with radiobuttons

--- components/product.js
+++ components/product.js
@@ -15,67 +15,80 @@ Vue.component('product-details', {
 
 Vue.component('product-review', {
   template: `
-    <form class="review-form" @submit.prevent="onSubmit">
+    <div>
+      <form class="review-form" @submit.prevent="onSubmit">
 
-      <p v-if="errors.length">
-        <b>Please correct the following error(s):</b>
-        <ul>
-          <li v-for="error in errors">{{ error }}</li>
-        </ul>
-      </p>
+        <p v-if="errors.length">
+          <b>Please correct the following error(s):</b>
+          <ul>
+            <li v-for="error in errors">{{ error }}</li>
+          </ul>
+        </p>
 
-      <p>
-        <label for="name">Name:</label>
-        <input type="text" id="name" v-model="name">
-      </p>
+        <p>
+          <label for="name">Name:</label>
+          <input type="text" id="name" v-model="name">
+        </p>
 
-      <p>
-        <label for="review">Review:</label>
-        <textarea id="review" v-model="review"></textarea>
-      </p>
+        <p>
+          <label for="review">Review:</label>
+          <textarea id="review" v-model="review"></textarea>
+        </p>
 
-      <p>
-        <label for="rating">Rating:</label>
-        <select id="rating" v-model.number="rating">
-          <option>5</option>
-          <option>4</option>
-          <option>3</option>
-          <option>2</option>
-          <option>1</option>
-        </select>
-      </p>
+        <p>
+          <label for="rating">Rating:</label>
+          <select id="rating" v-model.number="rating">
+            <option>5</option>
+            <option>4</option>
+            <option>3</option>
+            <option>2</option>
+            <option>1</option>
+          </select>
+        </p>
 
-      <p>
-        <input type="submit" value="Submit">
-      </p>
+        <p>Would you recommend this product ?</p>
+        <input type="radio" value="yes" v-model="recommend">
+        <label for="yes">Yes</label><br>
+        <input type="radio" value="no" v-model="recommend">
+        <label for="no">No</label><br>
 
-    </form>
+        <p>
+          <input type="submit" value="Submit">
+        </p>
+
+      </form>
+
+    </div>
   `,
   data() {
     return {
       name: null,
       review: null,
       rating: null,
-      errors: []
+      errors: [],
+      recommend: null
     }
   },
   methods: {
     onSubmit() {
-      if (this.name && this.review && this.rating) {
+      if (this.name && this.review && this.rating && this.recommend) {
         let productReview = {
           name: this.name,
           review: this.review,
-          rating: this.rating
+          rating: this.rating,
+          recommend: this.recommend
         }
         this.$emit('review-submitted', productReview)
         this.name = null
         this.review = null
         this.rating = null
+        this.recommend = null
       }
       else {
         if(!this.name) this.errors.push("Name required.")
         if(!this.review) this.errors.push("Review required.")
         if(!this.rating) this.errors.push("Rating required.")
+        if(!this.recommend) this.errors.push("Recommend required.")
       }
     }
   }
